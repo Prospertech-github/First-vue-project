11c34a3b4953b9fec2ce88d808a8827a1a97aa94
Prosper Ikechukwu <mrhendrix62@gmail.com>
2022-09-19 13:58:21 +0100

    FEAT: Restructured codes to align UI rendering with the Json server

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,48 @@ export default {
 		this.tasks = await this.fetchTasks();
 	},
 	methods: {
-		addTask(task) {
-			this.tasks = [...this.tasks, task];
+		async addTask(task) {
+			const res = await fetch('http://localhost:5000/tasks', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+				},
+				body: JSON.stringify(task),
+			});
+
+			const data = await res.json();
+			this.tasks = [...this.tasks, data];
 		},
-		deleteTask(id) {
+		async deleteTask(id) {
 			if (confirm('You are about to delete a task')) {
-				this.tasks = this.tasks.filter(task => task.id !== id);
+				const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+					method: 'DELETE',
+				});
+
+				res.status === 200
+					? (this.tasks = this.tasks.filter(task => task.id !== id))
+					: alert('Error deleting Task');
 			}
 		},
-		toggleReminder(id) {
+		async toggleReminder(id) {
+			const taskToToggle = await this.fetchTask(id);
+
+			const updateTask = {
+				...taskToToggle,
+				reminder: !taskToToggle.reminder,
+			};
+			const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+				method: 'PUT',
+				headers: {
+					'Content-type':'application/json'
+				},
+				body: JSON.stringify(updateTask)
+			});
+
+			const data = await res.json();
+
 			this.tasks = this.tasks.map(task =>
-				task.id == id ? { ...task, reminder: !task.reminder } : task
+				task.id == id ? { ...task, reminder: data.reminder} : task
 			);
 		},
 		toggleAddTask() {
@@ -41,14 +72,14 @@ export default {
 			const res = await fetch('http://localhost:5000/tasks');
 			const tasks = await res.json();
 
-			return tasks
+			return tasks;
 		},
 		async fetchTask(id) {
 			const res = await fetch(`http://localhost:5000/tasks/${id}`);
-			const task = await res.json;
+			const task = await res.json();
 
-			return task
-		}
+			return task;
+		},
 	},
 };
 </script>
